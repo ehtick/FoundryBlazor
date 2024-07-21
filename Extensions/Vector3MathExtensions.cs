f72c5381d83541743a9d2d1abc11ebb85c3b4137
Steve <stephen.r.strong@saic.com>
2024-07-21 16:06:21 -0400

    Refactor Vector3MathExtensions class for improved code organization and functionality

--- Extensions/Vector3MathExtensions.cs
+++ Extensions/Vector3MathExtensions.cs
@@ -1,6 +1,5 @@
 ﻿
 using BlazorThreeJS.Maths;
-using System.Linq.Dynamic.Core.CustomTypeProviders;
 
 namespace FoundryBlazor.Extensions;
 
@@ -62,6 +61,59 @@ public static class Vector3MathExtensions
             Z = Math.Abs(a.Z - b.Z)
         };
     }
+
+    // distance between 2 vectors
+    public static double Distance(this Vector3 a, Vector3 b)
+    {
+        return Math.Sqrt(
+            Math.Pow(a.X - b.X, 2) +
+            Math.Pow(a.Y - b.Y, 2) +
+            Math.Pow(a.Z - b.Z, 2)
+        );
+    }
+
+    // dot product of 2 vectors
+    public static double Dot(this Vector3 a, Vector3 b)
+    {
+        return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+    }
+
+    // cross product of 2 vectors
+    public static Vector3 Cross(this Vector3 a, Vector3 b)
+    {
+        return new Vector3
+        {
+            X = a.Y * b.Z - a.Z * b.Y,
+            Y = a.Z * b.X - a.X * b.Z,
+            Z = a.X * b.Y - a.Y * b.X
+        };
+    }
+
+    // normalize vector
+    public static Vector3 Normalize(this Vector3 a)
+    {
+        var length = Math.Sqrt(a.X * a.X + a.Y * a.Y + a.Z * a.Z);
+        return new Vector3
+        {
+            X = a.X / length,
+            Y = a.Y / length,
+            Z = a.Z / length
+        };
+    }
+
+    //euler angles to vector
+    
+
+    // //transform vector by matrix
+    // public static Vector3 ApplyMatrix4(this Vector3 v, Matrix4x4 m)
+    // {
+    //     return new Vector3
+    //     {
+    //         X = v.X * m.Elements[0] + v.Y * m.Elements[4] + v.Z * m.Elements[8] + m.Elements[12],
+    //         Y = v.X * m.Elements[1] + v.Y * m.Elements[5] + v.Z * m.Elements[9] + m.Elements[13],
+    //         Z = v.X * m.Elements[2] + v.Y * m.Elements[6] + v.Z * m.Elements[10] + m.Elements[14]
+    //     };
+    // }
 
 
 }
